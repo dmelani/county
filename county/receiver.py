79ff8ebf98d77fa1f3a45d315255a133871b82a6
Daniel Melani <daniel.melani@gmail.com>
2012-04-19 14:00:17 +0200

    basic udp receiver created

---
+++ county/receiver.py
@@ -0,0 +1,19 @@
+import Queue
+import time
+import SocketServer
+
+class ReceiverHandler(SocketServer.BaseRequestHandler):
+	def handle(self):
+		print "received udp packet"
+		self.server.queue.put_nowait('derp')	
+		print "qsize:", self.server.queue.qsize()
+	
+class Receiver(SocketServer.UDPServer):
+	def __init__(self, server_address, RequestHandlerClass, queue):
+		self.queue = queue
+		SocketServer.UDPServer.__init__(self,server_address,RequestHandlerClass)
+
+if __name__ == '__main__':
+	queue = Queue.Queue()
+	server = Receiver(('localhost', 9999), ReceiverHandler, queue)
+	server.serve_forever()
