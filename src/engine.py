5be741643a94aac9ed5da7734eb5a172ecaf8491
Daniel Melani <daniel.melani@gmail.com>
2012-04-21 18:35:21 +0200

    terra instantiatetd

--- src/engine.py
+++ src/engine.py
@@ -1,20 +1,23 @@
-import firmament
 import pygame
 from pygame.locals import *
 import time
 import sys
+import firmament
+import terra
 
 class Engine(object):
 	def __init__(self, me_q):
 		print 'Engine starting...'
 		self.me_q = me_q
 		self.firmament = firmament.Firmament()
+		self.terra = terra.Terra()
 		self.done = False
 
 	def run(self):
 		while not self.done:
 			#do everything
 			self.firmament.clear()
+			self.terra.the_lone_range_rides_again()
 			self.handle_events()
 			pygame.display.flip()
 		print "Engine stopping..."
